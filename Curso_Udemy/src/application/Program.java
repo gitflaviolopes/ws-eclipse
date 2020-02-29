4e8375fffbbcd069a85b828794e4518bb704d45e
dev_flavio_lopes <flavio.lopes@cea.com.br>
2020-02-29 00:12:24 -0300

    Composição, enumeradores e datas

--- Curso_Udemy/src/application/Program.java
+++ Curso_Udemy/src/application/Program.java
@@ -1,21 +1,159 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 import entities.Account;
+import entities.Department;
 import entities.Employee;
+import entities.HourContract;
+import entities.Order;
 import entities.Product;
 import entities.Rent;
+import entities.Worker;
+import entities.enums.OrderStatus;
+import entities.enums.WorkerLevel;
 
 public class Program {
 
 	public static void main(String[] args) {
 		//CHAMAR O MÉTODO PARA O CODIGO A SER TESTADO;
-		Matriz2();
+		Composicao();
+	}
+	
+	public static void Composicao() {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Enter department's name: ");
+		String departmentName = sc.nextLine();
+		System.out.println("Enter worker data:");
+		System.out.print("Name: ");
+		String workerName = sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+		
+		System.out.print("How many contracts to this worker? ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Enter contract #" + i + " data:");
+			System.out.print("Date (DD/MM/YYYY): ");
+			Date contractDate = null;
+			try {
+				contractDate = sdf.parse(sc.next());
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = sc.nextInt();
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			worker.addContract(contract);
+		}
+		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+		
+		sc.close();
+	}
+	
+	public static void Enumeradores() {
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+	}
+	
+	public static void CalendarDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		System.out.println(sdf.format(d));
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		System.out.println(sdf.format(d));
+		
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = 1 + cal.get(Calendar.MONTH);
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
+	}
+	
+	public static void DateDemo() {
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		Date x1 = new Date();
+		Date x2 = new Date(System.currentTimeMillis());
+		Date x3 = new Date(0L);
+		Date x4 = new Date(1000L * 60L * 60L * 5L);
+		Date y1 = null;
+		try {
+			y1 = sdf1.parse("25/06/2018");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Date y2 = null;
+		try {
+			y2 = sdf2.parse("25/06/2018 15:42:07");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		System.out.println("x1: " + x1);
+		System.out.println("x2: " + x2);
+		System.out.println("x3: " + x3);
+		System.out.println("x4: " + x4);
+		System.out.println("y1: " + y1);
+		System.out.println("y2: " + y2);
+		System.out.println("y3: " + y3);
+		System.out.println("-------------");
+		System.out.println("x1: " + sdf2.format(x1));
+		System.out.println("x2: " + sdf2.format(x2));
+		System.out.println("x3: " + sdf2.format(x3));
+		System.out.println("x4: " + sdf2.format(x4));
+		System.out.println("y1: " + sdf2.format(y1));
+		System.out.println("y2: " + sdf2.format(y2));
+		System.out.println("y3: " + sdf2.format(y3));
+		System.out.println("-------------");
+		System.out.println("x1: " + sdf3.format(x1));
+		System.out.println("x2: " + sdf3.format(x2));
+		System.out.println("x3: " + sdf3.format(x3));
+		System.out.println("x4: " + sdf3.format(x4));
+		System.out.println("y1: " + sdf3.format(y1));
+		System.out.println("y2: " + sdf3.format(y2));
+		System.out.println("y3: " + sdf3.format(y3));
 	}
 	
 	public static void Matriz2() {
