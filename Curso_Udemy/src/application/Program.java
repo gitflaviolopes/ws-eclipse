a763558440054a2a23db30b68a89bdea15276e66
dev_flavio_lopes <flavio.lopes@cea.com.br>
2020-03-01 21:19:53 -0300

    Exercicios de composição

--- Curso_Udemy/src/application/Program.java
+++ Curso_Udemy/src/application/Program.java
@@ -13,10 +13,15 @@ import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 import entities.Account;
+import entities.Client;
+import entities.Comment;
 import entities.Department;
 import entities.Employee;
 import entities.HourContract;
 import entities.Order;
+import entities.OrderEx3;
+import entities.OrderItem;
+import entities.Post;
 import entities.Product;
 import entities.Rent;
 import entities.Worker;
@@ -27,7 +32,100 @@ public class Program {
 
 	public static void main(String[] args) {
 		//CHAMAR O MÉTODO PARA O CODIGO A SER TESTADO;
-		Composicao();
+		Composicao3();
+	}
+	
+	public static void Composicao3() {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = null;
+		try {
+			birthDate = sdf.parse(sc.next());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		OrderEx3 order = new OrderEx3(new Date(), status, client);
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		for (int i=1; i<=n; i++) {
+			System.out.println("Enter #" + i + " item data:");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+
+			Product product = new Product(productName, productPrice);
+
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+
+			OrderItem orderItem = new OrderItem(quantity, productPrice, product); 
+
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		System.out.println("ORDER SUMMARY:");
+		System.out.println(order);
+		
+		sc.close();
+	}
+	
+	public static void Composicao2() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Comment c1 = new Comment("Have a nice trip!");
+		Comment c2 = new Comment("Wow that's awesome!");
+		Post p1 = null;
+		try {
+			p1 = new Post(
+					sdf.parse("21/06/2018 13:05:44"), 
+					"Traveling to New Zealand", 
+					"I'm going to visit this wonderful country!", 
+					12);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		Comment c3 = new Comment("Good night");
+		Comment c4 = new Comment("May the Force be with you");
+		Post p2 = null;
+		try {
+			p2 = new Post(
+					sdf.parse("28/07/2018 23:14:19"), 
+					"Good night guys", 
+					"See you tomorrow", 
+					5);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		p2.addComment(c3);
+		p2.addComment(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 	
 	public static void Composicao() {
--- Curso_Udemy/src/entities/Product.java
+++ Curso_Udemy/src/entities/Product.java
@@ -1,17 +1,27 @@
 package entities;
 
 public class Product {
-	String name;
-	private double price;
+	private String name;
+	private Double price;
 	
-	public Product() { }
-
-	public Product(String name, double price) {
+	public Product(String name, Double price) {
 		this.name = name;
 		this.price = price;
 	}
-	
-	public double getPrice() {
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
 		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 }
